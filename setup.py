da3a6ddd93acff40e0a4022e959234024742989d
Matheus Gil <matheussoprangil@gmail.com>
2024-10-27 15:36:49 -0400

    feat: implemented a system to ensure a default database is always created or used

--- setup.py
+++ setup.py
@@ -4,12 +4,25 @@ from database import db
 from views import ProfileView
 from models import Profile
 from app.router import init_router
+from sqlalchemy import select
+import datetime
 
 app = create_app_instance()
+
+def create_default_user(db, app):
+    #with app.app_context():
+    userAlreadyExists = db.session.execute(select(Profile.id).limit(1)).fetchone()
+    if not userAlreadyExists:
+        name = app.config.get('DEFAULT_ADMIN_NAME')
+        passw = app.config.get('DEFAULT_ADMIN_PASSWORD')
+        admail = app.config.get('DEFAULT_ADMIN_EMAIL')
+        db.session.add(Profile(id='1', username=name, password=passw, email=admail, registered= datetime.datetime.now()))
+        db.session.commit()
 
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
+        create_default_user(db, app)
 
         # Admin setup
         admin = Admin(app, name='Super App', template_mode='bootstrap4')
